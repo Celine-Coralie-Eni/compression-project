356866b64bfc3ed94d6eba240453312170ac2d24
Celine-Coralie-Eni <celinecoralie@icloud.com>
2025-04-22 20:25:40 +0100

    feature: lzf algorithm

---
+++ rust-compressor/src/lzf.rs
@@ -0,0 +1,14 @@
+use crate::lz::decompress;
+use crate::lz::compress;
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_lz_roundtrip() {
+        let input = b"ABABABABABAB";
+        let compressed = compress(input);
+        let decompressed = decompress(&compressed);
+        assert_eq!(input.to_vec(), decompressed);
+    }
+}
